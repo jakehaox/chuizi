d53629091d807b5ff15f47456525e349aa023757
haoxinning <954337612@qq.com>
2018-11-30 20:45:05 +0800

    2

---
+++ js/index-zhang.js
@@ -0,0 +1,16 @@
+// 张燕的js
+
+// input的js
+function toBtn(){
+	var oTxt = document.getElementById('txt');
+	oTxt.placeholder = '请输入搜索的商品';
+}
+
+var oTxt = document.getElementById('txt');
+oTxt.onclick = toBtn;
+
+// 热门商品的js
+
+function toRenMen(){
+	
+}
---
+++ js/index.js
@@ -0,0 +1,23 @@
+// 张燕的js
+
+// input的js
+window.onload = function(){
+
+	var oTxt = document.getElementById('txt');
+	oTxt.onclick = function(){
+		this.placeholder = '请输入搜索的商品';
+		// if (oTxt.placeholder == '请输入搜索的商品') {
+		// 	oTxt.placeholder = '';
+		// }else{
+		// 	oTxt.placeholder = '请输入搜索的商品';
+		// }
+	}
+	
+
+
+	// 热门商品的js
+
+	// function toRenMen(){
+		
+	// }
+}
