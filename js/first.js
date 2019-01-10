ceaadc700cb3c467ee36157e5ae90abada693cc2
haoxinning <954337612@qq.com>
2019-01-10 16:49:39 +0800

    4

--- js/first.js
+++ js/first.js
@@ -4,11 +4,23 @@
    处理导航栏部分
 */
 
-/* 坚果R1-设计 图片选项卡*/
 headleElecProduct();
 function headleElecProduct(){
-   var aDescribeList = document.querySelector('.describe .describe-list');
-   
+	//获取元素
+	var aTopNav1  = document.querySelectorAll('.top-nav1 .top-nav-item');
+	var aUserInfoList = document.querySelectorAll('.user-info-list .user-info-item');
+	//添加事件
+	for(var i = 0; i<aTopNav1.length;i++){
+        aTopNav1[i].index = i;
+        aTopNav1[i].onclick = function(){
+	       	for(var j = 0;j<aTopNav1.length;j++){
+	       		aTopNav1[i].style.color = 'white';
+       	        aTopNav1[i].style.fontWeight = 'bold';
+	            aTopNav1[j].className = 'top-nav-item';
+	       	}
+	       	this.className = 'top-nav-item checked';
+        }
+	}
 }
 
 window.onload = function(){
@@ -22,3 +34,6 @@ window.onload = function(){
 		oTankuang.style.display = 'none';
 	}
 }
+
+      
+	
