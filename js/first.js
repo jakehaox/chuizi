151f2f4863e169d0e9665ce40af97dd9e0dbe50a
haoxinning <954337612@qq.com>
2019-01-16 12:09:05 +0800

    2

--- js/first.js
+++ js/first.js
@@ -4,24 +4,39 @@
    处理导航栏部分
 */
 
-headleElecProduct();
-function headleElecProduct(){
-	//获取元素
-	var oDescribeList = document.querySelectorAll('.describe-list img');
-	var aLeftArrow = document.querySelector('.left-arrow');
-	var aRightArrow = document.querySelector('.right-arrow');
-	var aName = document.querySelector('.name');
-	var oTxt4 = document.querySelector('.txt4');
-	var oTxt5 = document.querySelector('.txt5');
-	var aYear = document.querySelector('.year');
-	//添加事件
-	for(var i =0;i<oDescribeList.length;i++){
-		console.log(oDescribeList);
-	}
-	
+// headleElecProduct();
+// function headleElecProduct(){
+// 	//获取元素
+// 	var oDescribeList = document.querySelectorAll('.describe-list img');
+// 	var aLeftArrow = document.querySelector('.left-arrow');
+// 	var aRightArrow = document.querySelector('.right-arrow');
+// 	var aName = document.querySelector('.name');
+// 	var oTxt4 = document.querySelector('.txt4');
+// 	var oTxt5 = document.querySelector('.txt5');
+// 	var aYear = document.querySelector('.year');
+// 	//添加事件
+// 	aLeftArrow.oncilck = function(){
+// 		for(var i =0;i<oDescribeList.length;i++){
+// 		console.log(oDescribeList);
+// 	    }
+// 	}
 
+// }
+//处理顶部	
+var oUserInfo = document.getElementById('user-info');
+var oUserInfoRight = document.getElementById('user-info-right');
+window.onscroll = function(){
+	getScrollTop();
+	if(getScrollTop() >= 100){
+		oUserInfo.style.position = 'fixed';
+		oUserInfo.style.top = '0px';
+	}else{
+        oUserInfo.style.position = '';
+	}
+}
+// 顶部导航购物车js
 window.onload = function(){
-	// 顶部导航购物车js
+	
 	var oLink = document.getElementById('link');
 	var oTankuang = document.getElementById('tankuang');
 	oLink.onmouseover = function(){
@@ -30,4 +45,4 @@ window.onload = function(){
 	oLink.onmouseout = function(){
 		oTankuang.style.display = 'none';
 	}
-}	
+}
